8c18e805435fd060015e5f1f9d133147bcee2c99
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-10 19:30:07 +0200

    Do task 3, project  0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for converting a float to a string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Parameters:
+    n (float): The float number to convert.
+
+    Returns:
+    str: The string representation of the float n.
+    """
+    return str(n)
