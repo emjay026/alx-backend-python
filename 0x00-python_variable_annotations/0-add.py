af3a9432c157294a9a4b69c460c18f12e842929d
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-10 18:43:44 +0200

    Do task 0, project  0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for adding two numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers.
+
+    Parameters:
+    a (float): First number.
+    b (float): Second number.
+
+    Returns:
+    float: Sum of a and b.
+    """
+    return a + b
