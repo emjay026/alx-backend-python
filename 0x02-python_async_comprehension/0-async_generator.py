6624baf9969f4d1a1b535463468761e9b62bae83
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-15 12:51:15 +0200

    Do task 0, project 0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -19,4 +19,4 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.randint(0, 10)  # Yield a random number between 0 and 10
+        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
