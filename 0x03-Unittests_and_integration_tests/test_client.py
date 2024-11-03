aa5a476969224b1aed8968224ee6a88c07bf8d50
emjay026 <mojalefa.emjay01@gmail.com>
2024-11-03 20:11:01 +0200

    Do task 7, project 0x03. Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -96,6 +96,25 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test the has_license method.
+
+        Parameters
+        ----------
+        repo: Dict
+            Repository dictionary containing license information.
+        license_key: str
+            The license key to check.
+        expected: bool
+            The expected return value of the has_license method.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
